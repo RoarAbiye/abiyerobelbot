35d96b490f0529b5e79a852bc64cff94edd4198d
robelabiye <rblabiye5@gmail.com>
2023-05-05 10:54:45 +0300

    Changed URL to netlify

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require("dotenv").config()
 
-const express = require("express")
-const bodyParser = require("body-parser")
-const axios = require("axios")
+import express from "express"
+import { json } from "body-parser"
+import { get, post } from "axios"
 
 const { TOKEN, SERVER_URL } = process.env
 const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`
@@ -10,10 +10,10 @@ const URI = `/webhook/${TOKEN}`
 const WEBHOOK_URL = SERVER_URL   + URI
 
 const app = express()
-app.use(bodyParser.json())
+app.use(json())
 
 const init = async () => {
-  const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`)
+  const res = await get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`)
   console.log(res.data)
 }
 
@@ -23,12 +23,13 @@ app.post(URI, async (req, res) => {
   const chatId = req.body.message.chat.id;
   const text = req.body.message.text;
 
-  await axios.post(`${TELEGRAM_API}/sendMessage`, {
+  await post(`${TELEGRAM_API}/sendMessage`, {
     chat_id: chatId,
     text: text
   })
   return res.send()
 })
+
 app.listen(process.env.PORT || 5000, async () => {
   console.log('🚀 app running on port', process.env.PORT || 5000);
   await init();
